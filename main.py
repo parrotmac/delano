a1fd1008bcc7913cdc1daf5c9a55f3169e8a90f6
Isaac Parker <parrotmac@gmail.com>
2017-06-20 18:06:44 -0600

    Removing port hardcoding

--- main.py
+++ main.py
@@ -1,37 +1,45 @@
-import base64
-
+import argparse
 import serial
 
-serial_address = "/dev/cu.usbserial-STY0CNSO"
-baud_rate = 115200
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--port')
+parser.add_argument('-b', '--baud', default=115200)
+args = parser.parse_args()
 
-conn = serial.Serial(serial_address, baud_rate)
+if not args.port:
+    print("Please specify a serial port with --port")
+    print("For a list of available ports run `python -m serial.tools.list_ports`")
+    exit(-1)
+
+
+conn = serial.Serial(args.port, args.baud)
 
 if conn.in_waiting:
     print(conn.read_all())
 
 
-
-
-
 # <---------->
 # BEGIN: ATD
 
-conn.write("ATD\r")
-conn.flush()
+def initialize_cmd_ATD():
+    conn.write("ATD\r")
+    conn.flush()
 
-buffer = ""
-character_found = False
-while(not character_found):
-    buffer += conn.read_all()
-    if ">" in buffer:
-        character_found = True
+    buffer = ""
+    character_found = False
+    while (not character_found):
+        buffer += conn.read_all()
+        if ">" in buffer:
+            character_found = True
 
-cleaned_buffer = buffer.split('\r')
+    cleaned_buffer = buffer.split('\r')
 
-if "ATD" not in cleaned_buffer or "OK" not in cleaned_buffer or ">" not in cleaned_buffer:
-    print("Device not reset")
-    exit(-1)
+    if "ATD" not in cleaned_buffer or "OK" not in cleaned_buffer or ">" not in cleaned_buffer:
+        raise Exception("Device not reset")
+
+
+initialize_cmd_ATD()
+
 print("Device settings reset")
 
 # END: ATD
@@ -112,6 +120,9 @@ def get_pid(pid):
 
         new_buffer.append(buffer_item.strip())
 
+    if "NO DATA" in new_buffer:
+        raise Exception("No data available from device")
+
     return new_buffer
 
 def format_pid_response_line(pid_response_line):
@@ -134,18 +145,46 @@ supported_query_pids = [
     "C0"
 ]
 
-supported_pid_maps = []
+supported_pid_list = []
 
 for support_query_index in supported_query_pids:
-    vehicle_response = get_pid("01" + support_query_index)
 
-    formatted_response = format_pid_response_line(vehicle_response[0])
+    try:
+        vehicle_response = get_pid("01" + support_query_index + "1")
+
+        formatted_response = format_pid_response_line(vehicle_response[0])
+
+        print(formatted_response)
+
+        range_start = int(formatted_response[0], base=16)
+
+        support_binary_encoded_string = "".join(formatted_response[1])
+
+        support_map = bin(int(support_binary_encoded_string, base=16)).lstrip('0b')
+
+        current_support_map_index = 0
+        for pid_index in range(range_start, range_start + 32):
+            if support_map[current_support_map_index] == "1":
+                supported_pid_list.append("{0:02x}".format(pid_index))
+                print("Supports PID {0:02x}".format(pid_index))
+            current_support_map_index += 1
+
+    except Exception as e:
+        print(e)
+
+
+for n in range(20):
+    for supported_pid in supported_pid_list:
+        try:
+            print(format_pid_response_line(get_pid("01" + supported_pid)[0]))
+        except Exception as e:
+            supported_pid_list.remove(supported_pid)
+            print(e)
 
 
 
 
+# Home-brew debugging!
 
-
-
-import code
-code.interact(local=dict(globals(), **locals()))
+# import code
+# code.interact(local=dict(globals(), **locals()))
